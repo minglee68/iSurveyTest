62b15b232fdedf6a9f5c904a326c1873eb7b5c53
Min Young Lee <avadaisuki68@gmail.com>
2018-07-17 21:11:52 +0900

    iSurvey

--- iSurvey/app.js
+++ iSurvey/app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 
 var app = express();
@@ -21,8 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', loginRouter);
-app.use('/index', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
